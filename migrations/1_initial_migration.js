74630362e340d0b026a28c57237c90ba64b0a21c
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-07-22 16:50:23 +0700

    deploy casino migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var Casino = artifacts.require("./Casino.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Casino);
 };
