23ccf463f5fe770e79f92b9a8f0691d1fdd0cf38
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-07-29 22:22:19 +0700

    set complete visible

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -12,7 +12,7 @@ contract Migrations {
     owner = msg.sender;
   }
 
-  function setCompleted(uint completed) restricted {
+  function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
 
