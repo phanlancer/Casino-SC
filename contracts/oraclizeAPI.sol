ca876c275fc221a3615818973a5e7235e7f5a776
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-08-20 19:25:15 +0700

    add memory prefix

--- contracts/oraclizeAPI.sol
+++ contracts/oraclizeAPI.sol
@@ -37,7 +37,8 @@ contract OraclizeI {
   function query(uint _timestamp, string calldata _datasource, string calldata _arg) external payable returns (bytes32 _id);
   function query_withGasLimit(uint _timestamp, string calldata _datasource, string calldata _arg, uint _gaslimit)
             external payable returns (bytes32 _id);
-  function query2(uint _timestamp, string _datasource, string _arg1, string _arg2) public payable returns (bytes32 _id);
+  function query2(uint _timestamp, string memory _datasource, string memory _arg1, string memory _arg2)
+            public payable returns (bytes32 _id);
   function query2_withGasLimit(uint _timestamp, string _datasource, string _arg1, string _arg2, uint _gaslimit)
             external payable returns (bytes32 _id);
   function queryN(uint _timestamp, string _datasource, bytes _argN) public payable returns (bytes32 _id);
