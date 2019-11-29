bd66d617b4d8c842c81bd7a1b45850ed0d5efd0f
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-11-29 19:20:56 +0700

    add memory

--- contracts/oraclizeAPI.sol
+++ contracts/oraclizeAPI.sol
@@ -524,7 +524,7 @@ contract usingOraclize {
     return oraclize_query(datasource, dynargs, gaslimit);
   }
 
-  function oraclize_query(string datasource, string[4] args) internal oraclizeAPI returns (bytes32 id) {
+  function oraclize_query(string memory datasource, string[4] memory args) internal oraclizeAPI returns (bytes32 id) {
     string[] memory dynargs = new string[](4);
     dynargs[0] = args[0];
     dynargs[1] = args[1];
@@ -532,7 +532,7 @@ contract usingOraclize {
     dynargs[3] = args[3];
     return oraclize_query(datasource, dynargs);
   }
-  function oraclize_query(uint timestamp, string datasource, string[4] args) internal oraclizeAPI returns (bytes32 id) {
+  function oraclize_query(uint timestamp, string memory datasource, string[4] memory args) internal oraclizeAPI returns (bytes32 id) {
     string[] memory dynargs = new string[](4);
     dynargs[0] = args[0];
     dynargs[1] = args[1];
