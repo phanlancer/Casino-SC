836f9b1caf53217ff97f093c2a5b3ff8727d3073
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-12-02 09:52:44 +0700

    update code

--- contracts/oraclizeAPI.sol
+++ contracts/oraclizeAPI.sol
@@ -549,7 +549,7 @@ contract usingOraclize {
     dynargs[3] = args[3];
     return oraclize_query(timestamp, datasource, dynargs, gaslimit);
   }
-  function oraclize_query(string datasource, string[4] args, uint gaslimit) internal oraclizeAPI returns (bytes32 id) {
+  function oraclize_query(string memory datasource, string[4] memory args, uint gaslimit) internal oraclizeAPI returns (bytes32 id) {
     string[] memory dynargs = new string[](4);
     dynargs[0] = args[0];
     dynargs[1] = args[1];
