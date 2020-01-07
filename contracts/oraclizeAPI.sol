a0dd918b9a4676642cc1d73c43c1b556f55bdab3
Phan Trung Sinh <phanlancerduk@hotmail.com>
2020-01-07 20:41:48 +0700

    add memory params

--- contracts/oraclizeAPI.sol
+++ contracts/oraclizeAPI.sol
@@ -696,7 +696,7 @@ contract usingOraclize {
     return oraclize_query(datasource, dynargs, gaslimit);
   }
 
-  function oraclize_query(string datasource, bytes[4] args) internal oraclizeAPI returns (bytes32 id) {
+  function oraclize_query(string memory datasource, bytes[4] memory args) internal oraclizeAPI returns (bytes32 id) {
     bytes[] memory dynargs = new bytes[](4);
     dynargs[0] = args[0];
     dynargs[1] = args[1];
